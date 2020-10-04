d7ce63e27e1055c79f9133c021ee839741be17f5
clintonasoh <clintonasoh@gmail.com>
2020-10-04 14:03:16 +0100

    Feat: First try for both Hyperdrive and AutoML

--- train.py
+++ train.py
@@ -9,27 +9,9 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-from azureml.core import Dataset, Datastore
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-# ds = ### YOUR CODE HERE ###
-#ds = TabularDatasetFactory.from_demilited_files(path=web_path, header=True)
-
-web_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-ds = Dataset.Tabular.from_delimited_files(path=web_path, header=True)
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
-run = Run.get_context()
-
+# Leaving the TODOs here as in the starter code cause errors in my notebook
+# So, I had to rearrange the structure of the code in this file to get it to run in the notebook.
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -56,6 +38,23 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+# ds = ### YOUR CODE HERE ###
+web_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+#ds = Dataset.Tabular.from_delimited_files(path=web_path, header=True)
+ds = TabularDatasetFactory.from_delimited_files(path=web_path, header=True, validate=True, include_path=False, separator=',')
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+### YOUR CODE HERE ###
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=40)
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
